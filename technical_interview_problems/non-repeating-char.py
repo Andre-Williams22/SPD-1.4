662e99d3f13b3267490341180d916aa509177c3f
Andre Williams <awjrs22@gmail.com>
2020-04-01 12:26:59 -0700

    adds optimal solution on 0(n)

--- technical_interview_problems/non-repeating-char.py
+++ technical_interview_problems/non-repeating-char.py
@@ -5,3 +5,29 @@ Return the first unique character in the string. If there is no unique item retr
 # Clarifying Questions: is the function taking 1 word at a time? What does the function return? Can there be two characters that appear once?
 # Assumptions: The inputs are strings only, the function returns the unique character.
 # Possible Solutions: A hashtable that stores the values and a list that keeps track of all unique characters
+
+
+def non_repeating(string):
+  hashtable = {}
+  nums = []
+  for item in string:
+    if item in hashtable:
+      hashtable[item] += 1
+    else:
+      hashtable[item] = 1
+  
+  for item in hashtable:
+    if hashtable[item] == 1:
+      nums.append(item)
+    
+  if len(nums) == 0:
+    return None
+
+  return nums[0]
+
+
+
+word = 'aabbdbc'
+word2 = 'xxbyzb'
+print(non_repeating(word))
+print(non_repeating(word2))
