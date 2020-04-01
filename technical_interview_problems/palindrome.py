cc4f664bf3ab429195db25bdf6c6e7904917bd8a
Andre Williams <awjrs22@gmail.com>
2020-04-01 12:21:08 -0700

    adds a possible solution

--- technical_interview_problems/palindrome.py
+++ technical_interview_problems/palindrome.py
@@ -7,3 +7,12 @@
 
 
 
+def palindrome(string):
+  for i in range(0, len(string)//2):
+    if string[i] != string[len(string)-i-1]:
+      return False
+  return True
+
+print(palindrome('cat'))
+print(palindrome('alla'))
+print(palindrome('ot to'))
