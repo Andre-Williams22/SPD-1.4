bc3410a6c5e88362f8f2f6a43f41a6c545a13511
Andre Williams <awjrs22@gmail.com>
2020-04-01 12:19:51 -0700

    adds first problem

---
+++ technical_interview_problems/palindrome.py
@@ -0,0 +1,9 @@
+# Problem: Write a function to determine if a string is a palindrome or not
+
+# Clarifying Questions: is the function taking 1 word at a time? What does the function return? string or boolean  
+# Assumptions: The inputs are strings only, the function returns a boolean
+# Possible Solutions: String slicing on both ends of the string to see if they match, pointers on each end to check similarities, 
+# using sets to see what the elements have in commmon 
+
+
+
