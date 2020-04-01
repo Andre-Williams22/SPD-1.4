f779317ffe88554ea4ebc812fa7ea891722577aa
Andre Williams <awjrs22@gmail.com>
2020-04-01 12:26:22 -0700

    adds second problem

---
+++ technical_interview_problems/non-repeating-char.py
@@ -0,0 +1,7 @@
+''' problem: implement a function that takes a string and returns a character that only appears once. 
+Return the first unique character in the string. If there is no unique item retrun None.''' 
+
+
+# Clarifying Questions: is the function taking 1 word at a time? What does the function return? Can there be two characters that appear once?
+# Assumptions: The inputs are strings only, the function returns the unique character.
+# Possible Solutions: A hashtable that stores the values and a list that keeps track of all unique characters
