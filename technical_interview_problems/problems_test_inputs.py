aea0a6887f54868eea0858a9b75326d0e41d9a0a
Andre Williams <awjrs22@gmail.com>
2020-04-20 08:40:24 -0700

    adds second question with a good and bad test case

--- technical_interview_problems/problems_test_inputs.py
+++ technical_interview_problems/problems_test_inputs.py
@@ -1,5 +1,7 @@
 ''' given a sequence of brackets, how will you identify if it's valid or not. Every opening has a closing bracket'''
 
+# Question 1 
+
 def is_valid(string):
   stack = []
   hashtable = {'(':')', '[':']', '{':'}'}
@@ -17,3 +19,24 @@ print(is_valid('[{}]'))
 
 # Bad Input 
 print(is_valid(_[]))
+
+
+
+# Question 2 
+
+def element_mult_add(x, y):
+  ''' calculate the product of two list and then find the sum of the numbers in the list'''
+  n = [i*j for i,j in zip(x,y)]
+  count = 0
+  for item in n:
+    count += item
+  return count 
+
+
+x = [1, 2, 3]
+y = [10, 15, 20]
+# good test case input 
+print(element_mult_add(x, y))
+
+# bad test case input 
+print(element_mult_add(+,-))
