9738ef27cd5def87edfea9b3fc4724c970839b83
Andre Williams <awjrs22@gmail.com>
2020-05-04 12:18:43 -0700

    adds space and time analysis

--- technical_interview_problems/common_two_sorted_arrays.py
+++ technical_interview_problems/common_two_sorted_arrays.py
@@ -7,20 +7,19 @@
 # c = [1, 4, 9]
 
 def common_elements(list1, list2): # O(max(list1,list2))
-    new_list = []
+    new_list = [] # O(n) space complexity
     p1 = 0
     p2 = 0 
     # for i in range(0, len(list1)-1): #O(n)
-    while p1 < len(list1) and p2 < len(list2): #O(list1*list2)
-        if list1[p1] == list2[p2]: # 
-            new_list.append(list1[p1])
-            p1+=1 
-            p2+=1
-        elif list1[p1] > list2[p2]:
-            p2+=1
-        else:
-            p1+=1
-            
+    while p1 < len(list1) and p2 < len(list2): #O(list1*list2) time complexity
+        if list1[p1] == list2[p2]: # O(1)
+            new_list.append(list1[p1]) # O(1)
+            p1+=1 # O(1)
+            p2+=1 # O(1)
+        elif list1[p1] > list2[p2]: # O(1)
+            p2+=1 # O(1) 
+        else: # O(1)
+            p1+=1 # O(1)
 
     return new_list
 
