ae882dfa82219a1eec44f7c58f58fadb74479b45
Andre Williams <awjrs22@gmail.com>
2020-05-04 12:17:11 -0700

    adds big O analysis and solves problem in optimal time

---
+++ technical_interview_problems/common_two_sorted_arrays.py
@@ -0,0 +1,29 @@
+# Problem: Given two ascending order arrays, print out the elements they have in common.
+# Example 
+# input:
+# a = [1, 4, 5, 6, 7, 9]
+# b = [1, 2, 4, 8, 9, 10]
+# Output:
+# c = [1, 4, 9]
+
+def common_elements(list1, list2): # O(max(list1,list2))
+    new_list = []
+    p1 = 0
+    p2 = 0 
+    # for i in range(0, len(list1)-1): #O(n)
+    while p1 < len(list1) and p2 < len(list2): #O(list1*list2)
+        if list1[p1] == list2[p2]: # 
+            new_list.append(list1[p1])
+            p1+=1 
+            p2+=1
+        elif list1[p1] > list2[p2]:
+            p2+=1
+        else:
+            p1+=1
+            
+
+    return new_list
+
+list1 = [1, 3, 6, 7, 10]
+list2 = [3, 4, 5, 9, 10]
+print(common_elements(list1, list2))
